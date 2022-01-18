4781b0918497cf9fa7c1eb7cfe4159f774082245
Noctussi <ahmetaus@gmail.com>
2022-01-19 00:58:05 +0300

    tried couple of changes but couldnt made it done. need to handle DOM

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
-const progressBar = document.querySelector("#progress-done-running");
+const progressBar = document.getElementById("progress-done-running");
 const dataDone = document.getElementById("progress-done-running").textContent;
 
 console.log(dataDone)
 
-progressBar.style.width = dataDone;
-progressBar.style.opacity = 1;
+progressBar.style.width = `${dataDone}`;
+progressBar.style.opacity = "1";
+
+// 2,67 - 3,17 - 3,05
