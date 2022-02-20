7310b93cfca177a4bfc8348c08985651cc828bad
Noctussi <ahmetaus@gmail.com>
2022-02-20 21:12:48 +0300

    koşu ve bisiklet verileri için array oluşturuldu. yüzdelik hesaplamalar script dosyasına aktarıldı.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
-const progressBar = document.getElementById("progress-done-running");
-const dataDone = document.getElementById("progress-done-running").textContent;
+const progressBarRunning = document.getElementById("progress-done-running");
+const progressBarCycling = document.getElementById("progress-done-cycling");
+const dataDoneCycling = document.getElementById("progress-done-cycling").textContent;
+const runningData = [2.67 , 3.17 , 3.05];
+const cyclingData = [11.57 , 12.98];
+const reducer = (accumulator, curr) => accumulator + curr;
 
-console.log(dataDone)
 
-progressBar.style.width = `${dataDone}`;
-progressBar.style.opacity = "1";
+document.getElementById("progress-done-running").textContent = `${Number((runningData.reduce(reducer)/500*100).toFixed(2))}` + "%" ;
+progressBarRunning.style.width = `${runningData.reduce(reducer)/500*100}` + "%" ; 
+progressBarRunning.style.opacity = "1";
+document.getElementById("progress-done-cycling").textContent = `${Number((cyclingData.reduce(reducer)/1500*100).toFixed(2))}` + "%" ;
+progressBarCycling.style.width = `${cyclingData.reduce(reducer)/1500*100}` + "%" ; 
+progressBarCycling.style.opacity = "1";
 
-// 2,67 - 3,17 - 3,05
+console.log(runningData.reduce(reducer));
+
+console.log(Number((cyclingData.reduce(reducer)/1500*100).toFixed(2)))
